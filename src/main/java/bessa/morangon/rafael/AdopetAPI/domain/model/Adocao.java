24110a678eba95e5a3ffbf2f79e10fc3d0df641b
Rafael Bessa <rafaelbessacivil@gmail.com>
2024-03-15 00:37:32 -0300

    Criando classe Adoção

---
+++ src/main/java/bessa/morangon/rafael/AdopetAPI/domain/model/Adocao.java
@@ -0,0 +1,29 @@
+package bessa.morangon.rafael.AdopetAPI.domain.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "adocao")
+public class Adocao {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "pet_id")
+    private Pet pet;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "tutor_id")
+    private Tutor tutor;
+    private LocalDate data;
+
+}
